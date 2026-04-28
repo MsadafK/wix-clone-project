782c5fd309a3bf0651df929776d0c5992e0ce5b8
MsadafK <mssadaf0@gmail.com>
2026-04-28 12:05:14 +0530

    fixed issue

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: import.meta.env.VITE_BASE_PATH || "/wix-clone-project",
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  
+  return {
+    plugins: [react(), tailwindcss()],
+    base: env.VITE_BASE_PATH || "/wix-clone-project",
+  };
 });
